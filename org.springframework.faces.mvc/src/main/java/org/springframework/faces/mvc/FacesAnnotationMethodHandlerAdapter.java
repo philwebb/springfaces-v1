02cd9a796ac8060a9350e0de51e35a3bdb7821f3
philw <philw@96f92a50-1e92-45f8-a6b0-eb54d5336e60>
2009-10-28 16:43:30 +0000

    Mainly work on method resolver
    git-svn-id: http://svn.xp-dev.com/svn/spring-faces-mvc/spring-faces-mvc/trunk@66 96f92a50-1e92-45f8-a6b0-eb54d5336e60

--- org.springframework.faces.mvc/src/main/java/org/springframework/faces/mvc/FacesAnnotationMethodHandlerAdapter.java
+++ org.springframework.faces.mvc/src/main/java/org/springframework/faces/mvc/FacesAnnotationMethodHandlerAdapter.java
@@ -15,15 +15,23 @@
  */
 package org.springframework.faces.mvc;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.faces.FacesMapping;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+import org.springframework.web.servlet.mvc.multiaction.InternalPathMethodNameResolver;
+import org.springframework.web.servlet.mvc.multiaction.MethodNameResolver;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  * Extension of {@link AnnotationMethodHandlerAdapter} to support JSF controllers.
@@ -34,6 +42,71 @@ public class FacesAnnotationMethodHandlerAdapter extends AnnotationMethodHandler
 
 	// FIXME doccomments
 	// FIXME support FaceContext param injection?
+
+	private UrlPathHelper urlPathHelper = new UrlPathHelper();
+
+	private MethodNameResolver methodNameResolver = new InternalPathMethodNameResolver();
+
+	private PathMatcher pathMatcher = new AntPathMatcher();
+
+	private final Map<Class<?>, RequestMappingMethodsResolver> methodResolverCache = new ConcurrentHashMap<Class<?>, RequestMappingMethodsResolver>();
+
+	private FacesHandlerAdapter facesHandlerAdapter;
+
+	public void setUrlPathHelper(UrlPathHelper urlPathHelper) {
+		super.setUrlPathHelper(urlPathHelper);
+		this.urlPathHelper = urlPathHelper;
+	}
+
+	public void setMethodNameResolver(MethodNameResolver methodNameResolver) {
+		super.setMethodNameResolver(methodNameResolver);
+		this.methodNameResolver = methodNameResolver;
+	}
+
+	public void setPathMatcher(PathMatcher pathMatcher) {
+		super.setPathMatcher(pathMatcher);
+		this.pathMatcher = pathMatcher;
+	}
+
+	protected boolean supportsFaces(Object handler) {
+		// FIXME option to disable this?
+		Class handlerClass = ClassUtils.getUserClass(handler);
+		boolean hasFacesAnnotation = AnnotationUtils.findAnnotation(handlerClass, FacesMapping.class) != null;
+		return hasFacesAnnotation;
+	}
+
+	public boolean supports(Object handler) {
+		return supportsFaces(handler) && super.supports(handler);
+	}
+
+	protected ModelAndView handleCreateView(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return super.handle(request, response, handler);
+	}
+
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		FacesHandler facesHandler = new AnnotatedMethodFacesHandlerAdapter(handler);
+		return facesHandlerAdapter.handle(request, response, facesHandler);
+	}
+
+	public void setFacesHandlerAdapter(FacesHandlerAdapter facesHandlerAdapter) {
+		this.facesHandlerAdapter = facesHandlerAdapter;
+	}
+
+	/**
+	 * Build a HandlerMethodResolver for the given handler type.
+	 */
+	private RequestMappingMethodsResolver getMethodResolver(Object handler) {
+		Class handlerClass = ClassUtils.getUserClass(handler);
+		RequestMappingMethodsResolver resolver = this.methodResolverCache.get(handlerClass);
+		if (resolver == null) {
+			resolver = new RequestMappingMethodsResolver(handlerClass, urlPathHelper, methodNameResolver, pathMatcher);
+			this.methodResolverCache.put(handlerClass, resolver);
+		}
+		return resolver;
+	}
 
 	/**
 	 * Adapter class to convert the annotated handler into a {@link FacesMapping}.
@@ -61,36 +134,6 @@ public class FacesAnnotationMethodHandlerAdapter extends AnnotationMethodHandler
 			System.out.println(outcome);
 			return "/test/test2";
 		}
-	}
-
-	private FacesHandlerAdapter facesHandlerAdapter;
-
-	protected boolean supportsFaces(Object handler) {
-		// FIXME option to disable this?
-		Class handlerClass = ClassUtils.getUserClass(handler);
-		boolean hasFacesAnnotation = AnnotationUtils.findAnnotation(handlerClass, FacesMapping.class) != null;
-		return hasFacesAnnotation;
-	}
-
-	public boolean supports(Object handler) {
-		return supportsFaces(handler) && super.supports(handler);
-	}
-
-	protected ModelAndView handleCreateView(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-
-		return super.handle(request, response, handler);
-	}
-
-	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-
-		FacesHandler facesHandler = new AnnotatedMethodFacesHandlerAdapter(handler);
-		return facesHandlerAdapter.handle(request, response, facesHandler);
-	}
-
-	public void setFacesHandlerAdapter(FacesHandlerAdapter facesHandlerAdapter) {
-		this.facesHandlerAdapter = facesHandlerAdapter;
 	}
 
 }
